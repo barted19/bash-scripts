e632dadbb8ac62cac3a4f5da288a9de710612f67
bart <bartlomiej.kowal147@gmail.com>
2025-01-14 20:33:12 +0100

    Added new script new_backup_checker.sh

---
+++ new_backup_checker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bckp="backup"
+
+if [ ! -d "$bckp" ]; then
+   echo "Tworzenie katalogu $bckp "
+   mkdir "$bckp"
+fi
+
+# Kopiowanie wszystkich plików .sh do katalogu backup
+
+echo "Kopowanie plikow wszystkich plikow z rozszezeniem '.sh'"
+cp *.sh "$bckp"
+
+# Logowanie daty i godziny wykonania
+
+echo "data wykonania backupu to $(date)." >> backup.log
+echo "Logowanie zakonczone sukcesem!"
+
+
